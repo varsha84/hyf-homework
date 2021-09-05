948f4149bbb50bd0367b3c3e31b4d56a7721acb9
varsha verma <varsha.verma85@gmail.com>
2021-09-05 23:15:27 +0200

    adding react week1 homework files

---
+++ react/week1/todo-list/src/getTaskList.js
@@ -0,0 +1,15 @@
+function getTasks(){
+    const tasks = [{name:"Get out of bed", deadline:"Wed Sep 13 2017"},
+                {name:"Brush teeth", deadline:"Thu Sep 14 2017"},
+                {name:"Eat breakfast", deadline:"Fri Sep 15 2017"},
+                {name:"Go for walk",deadline:"Fri Sep 15 2017"},
+                {name:"pick up kids", deadline:"Fri Sep 15 2017"},
+                {name:"Danish class", deadline:"Thu Sep 14 2021"},
+                {name:"HYF class", deadline:"Sun Sep 17 2021"},
+                {name:"yoga classes", deadline:"Sat Sep 16 2021"}
+
+            ]
+return tasks;
+}
+
+export default getTasks;
